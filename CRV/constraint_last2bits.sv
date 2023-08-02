2ee0f5219532d91b3f53c6efc564ecca96dc6b8b
venkata-p2498 <81845583+venkata-p2498@users.noreply.github.com>
2023-08-02 17:49:04 +0530

    Update constraint_last2bits.sv

--- CRV/constraint_last2bits.sv
+++ CRV/constraint_last2bits.sv
@@ -14,7 +14,7 @@ module tb;
 
   initial begin
   bus = new();
-    repeat(50) begin   // object bus will be randomized 50 times
+    repeat(10) begin   // object bus will be randomized 10 times
     
       //object is randomized using randomize() for (addr/data) till all the constraints are satisfied.
     success = bus.randomize();
@@ -23,3 +23,17 @@ module tb;
   end
 endmodule
 
+/*Output generated
+----------------------------------------------------------------------
+# KERNEL: addr : 1110011010001100 data : 10111001100000000001110010100101
+# KERNEL: addr : 111011001000100  data : 1001111000001011001001011010100
+# KERNEL: addr : 11101110111100   data : 1110100100100011111100110101101
+# KERNEL: addr : 1110101101110100 data : 11010001100100000010111101011000
+# KERNEL: addr : 1001100001100    data : 100001111110000011011011011000
+# KERNEL: addr : 110111000010100  data : 1111001001011000110110010111010
+# KERNEL: addr : 1011110100010100 data : 10100010011111101011011001111101
+# KERNEL: addr : 1111000100011000 data : 11110111110001110001010100100100
+# KERNEL: addr : 111001111110000  data : 1010110010110110111110110011001
+# KERNEL: addr : 111000010100000  data : 1111001000001110111110111001011
+------------------------------------------------------------------------
+*/
