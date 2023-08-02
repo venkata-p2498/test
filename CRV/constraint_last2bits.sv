4d4cf3b66947a48229f0e4ad4fec00ccca089901
venkata-p2498 <81845583+venkata-p2498@users.noreply.github.com>
2023-08-02 17:10:21 +0530

    Update constraint_last2bits.sv

--- CRV/constraint_last2bits.sv
+++ CRV/constraint_last2bits.sv
@@ -1,1 +1,22 @@
+// To demonstrate the last 2 bits of address pin to be '0'
+
+class Bus;
+  rand bit [15:0] addr;
+  rand bit [31:0] data;
+  
+  constraint word_align{ addr [1:0] == 2'b0; }
+endclass
+
+module tb;
+  Bus bus;
+  bit success;
+
+  initial begin
+  bus = new();
+    repeat(10) begin
+    success = bus.randomize();
+    $display("addr : %0b data : %0b", bus.addr, bus.data);
+    end
+  end
+endmodule
 
